4bf05c9ee1d99e4efd40417b3c1cb4cac6e19a1c
Leannald13 <leannald13@gmail.com>
2020-04-15 11:58:41 +0000

    putting html templates in place and connecting to mongodb

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'dataCentricCluster'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@datacentriccluster-snqmw.mongodb.net/dataCentricCluster?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+    
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+def index():
+    return render_template("base.html", review=mongo.db.review.find())
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
-
+        port=int(os.environ.get('PORT')),
+        debug=True)
