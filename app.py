4e067a601ec787ba3c3585cb0115aa4e13a47c62
Leannald13 <leannald13@gmail.com>
2020-05-05 20:35:45 +0000

    removed add review function and template

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import os
 import math
 from flask import Flask, render_template, redirect, request, url_for, session
@@ -18,19 +19,9 @@ mongo = PyMongo(app)
 def home_page():  
     return render_template("home.html", addboxset=mongo.db.addboxset.find())
 
-@app.route('/add_new') 
-def add_new():
-    return render_template("typeofshow.html", genre=mongo.db.genre.find())
-
-
 @app.route('/add_boxset') 
 def add_boxset():
     return render_template("addseries.html", addboxset=mongo.db.addboxset.find())
-
-@app.route('/add_user') 
-def add_user():
-    return render_template("userinfo.html", user=mongo.db.user.find())
-
 
 @app.route('/all_boxsets')
 def all_boxsets():
@@ -50,8 +41,6 @@ def insert_boxset():
     }
     addboxset.insert_one(boxset)  
     return redirect(url_for('all_boxsets'))
-
-
 
 @app.route('/edit_boxset/<boxset_id>')
 def edit_boxset(boxset_id):
@@ -79,34 +68,19 @@ def update_boxset(boxset_id):
     mongo.db.addboxset.update({"_id": ObjectId(boxset_id)}, boxset) 
     return redirect(url_for('view_boxset', cards_id=boxset_id))
     # a GET request means we want to return the html page
-    return render_template('addreview.html', boxset=boxset)
+    return render_template('allseries.html', boxset=boxset)
 
 
 @app.route('/view_boxset/<cards_id>', methods=["GET", "POST"])
 def view_boxset(cards_id):
     addboxset = mongo.db.addboxset
     cards = addboxset.find_one({"_id": ObjectId(cards_id)})
-    boxset_reviews = mongo.db.review.find({}, {"boxset_id": cards_id})
-    return render_template('view.html', cards=cards, reviews=boxset_reviews)
-
-
-@app.route('/add_review/<boxset_id>', methods=["GET", "POST"])
-def add_review(boxset_id):
-    boxset = mongo.db.addboxset.find_one({"_id": ObjectId(boxset_id)})
-    boxset_id = mongo.db.addboxset.find_one({"_id": ObjectId(boxset_id)})["_id"]
-    if request.method == "POST":
-        # a POST request means that we're being sent review data from a form
-        review = {
-            "review_rating": request.form.get("review_rating"),
-            "review_message": request.form.get("review_message"),
-        }
-        mongo.db.addboxset.update_one({"_id": ObjectId(boxset_id)}, {"$push": {"boxset_reviews": review}})
-        return redirect(url_for('view_boxset', cards_id=boxset_id))
-    # a GET request means we want to return the html page
-    return render_template('addreview.html', boxset=boxset)
+    return render_template('view.html', cards=cards)
 
 
 if __name__ == "__main__":
         app.run(host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
+
+    
