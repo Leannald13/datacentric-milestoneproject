3fa6e1bef05782d41c160a157eaa1a25c3feb785
Leannald13 <leannald13@gmail.com>
2020-05-10 08:50:21 +0000

    hiding env.py

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ["MONGO_DBNAME"] = "boxsetReviews" 
-os.environ["MONGO_URI"] = "mongodb+srv://root:r00tUser@datacentriccluster-snqmw.mongodb.net/boxsetReviews?retryWrites=true&w=majority" 
-
-from os import path
-if path.exists("env.py"):
-  import env 
